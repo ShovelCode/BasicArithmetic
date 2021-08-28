a2b91946ac5ba27e3ad8fbbbd35bb9060ad739c2
Christopher Ray <vde435@gmail.com>
2021-08-28 13:58:49 -0500

    Update

---
+++ Shapes/Shapes.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void rectangle();
+void oletter();
+
+int main ( void ){
+	rectangle();
+	oletter();		
+}
+
+void rectangle(){
+	printf("*********\n");
+	printf("*       *\n");
+	printf("*       *\n");
+
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*       *\n");
+
+	printf("*       *\n");
+	printf("*       *\n");
+	printf("*********\n");
+}
+
+void oletter(){
+	printf("   ***   \n");
+	printf(" *     * \n");
+	printf("*       *\n");
+
+	printf("*       *\n");
+	printf("*       *\n");
+}
