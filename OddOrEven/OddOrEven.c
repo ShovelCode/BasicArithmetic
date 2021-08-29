19a035bb6aa1ab802bed2df763c69c2173dc67b7
Christopher Ray <vde435@gmail.com>
2021-08-29 13:39:56 -0500

    Update
    OddOrEven added to.

--- OddOrEven/OddOrEven.c
+++ OddOrEven/OddOrEven.c
@@ -4,5 +4,9 @@ int main ( void ){
 	int num;
 	printf("Enter integer: \n");
 	scanf("%d", &num);
-	printf("The number: %d", num);
+	if(num % 2 == 0){
+		printf("The number %d is even.", num);
+	} else {
+		printf("The number %d is odd.", num);
+	}
 }
