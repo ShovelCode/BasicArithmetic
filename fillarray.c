393766831500203bbbddbcfd664be73c241b2116
Christopher Ray <vde435@gmail.com>
2021-09-15 00:57:26 -0500

    Update

--- fillarray.c
+++ fillarray.c
@@ -4,11 +4,13 @@ void printFirst(int*);
 
 int main(){
 
-	int x[] = {1,2,3};;
+	int x[] = {1,2,3,4,5,6,7,8,9,10};
 
 	printFirst(x);
 }
 
 void printFirst(int* x){
-	printf("%d",x[0]);
+	printf("%d\n",x[0]);
+	printf("%d\n",x[9]);
+	printf("%c\n", (char) 89);
 }
